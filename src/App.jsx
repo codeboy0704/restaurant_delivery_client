c4a906bb1a59d6d1d43db8dbc650c7153659cc40
Draigon07 <ian.flores2@utp.ac.pa>
2024-03-31 10:41:49 -0500

    Setting routes and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Cart from './pages/Cart'
+import PlaceHolder from './pages/PlaceHolder'
 
 function App() {
   return (
     <div className='app'>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+         <Route path='/cart' element={<Cart />} />
+         <Route path='/order' element={<PlaceHolder />} />
+      </Routes>
     </div>
   )
 }
