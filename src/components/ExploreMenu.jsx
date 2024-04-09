cb5ac9188772dc13dea1612a3b6cb5cb5e5a1222
Draigon07 <ian.flores2@utp.ac.pa>
2024-04-08 22:49:25 -0500

    setCategory restore

--- src/components/ExploreMenu.jsx
+++ src/components/ExploreMenu.jsx
@@ -3,6 +3,7 @@ import { menu_list } from '../assets/assets'
 import Slider from 'react-slick'
 import slideSettings from "../utils/slideSettings"
 function ExploreMenu({ category, setCategory }) {
+    console.log(category)
     return (
         <div className='flex flex-col gap-2' id='explore-menu'>
             <h1 className='text-[#262626] font-semibold text-[30px] text-center mb-[10px]'>Explore our menu</h1>
@@ -12,10 +13,10 @@ function ExploreMenu({ category, setCategory }) {
                     <Slider {...slideSettings} className='outline-none border-0'>
                         {menu_list.map((el, i) => {
                             return (
-                                <>
+                                <div onClick={()=> setCategory(el.menu_name)}>
                                     <img className={category === el.menu_name ? "select w-[7.8vw] min-w-[70px] pointer rounded-[50%]" : "w-[7.8vw] min-w-[80px] pointer rounded-xl"} src={el.menu_image} alt="" />
                                     <p className='mt-[5px] mx-5 my-1 text-[#747474] pointer text-[1.4vw, 12px] '>{el.menu_name}</p>
-                                </>
+                                </div>
                             )
                         })}
                     </Slider>
